219133746d3fe408246265bfbeaaaa1f123016e6
wsverzut <wsverzut@gmail.com>
2023-10-09 15:24:00 -0300

    Criação do user POST

---
+++ src/user/dtos/createUser.dto.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export interface CreateUserDto {
+  name: string;
+  email: string;
+  phone: string;
+  cpf: string;
+  password: string;
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateUserDto } from './dtos/createUser.dto';
+
+@Controller('user')
+export class UserController {
+  @Post()
+  async createUser(@Body() createUser: CreateUserDto) {
+    return { ...createUser, password: undefined };
+  }
+}
