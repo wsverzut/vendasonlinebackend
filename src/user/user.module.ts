6fad81b20266e07212a4cea6f2c1a7715ca22f14
wsverzut <wsverzut@gmail.com>
2023-11-07 15:08:44 -0300

    Retorno da férias

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
+import { UserEntity } from './interfaces/user.entity';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,38 @@
+import { hash } from 'bcrypt';
+import { Repository } from 'typeorm';
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(CreateUserDto: CreateUserDto): Promise<UserEntity> {
+    const saltOrRounds = 10;
+
+    const passwordHash = await hash(CreateUserDto.password, saltOrRounds);
+    const user: UserEntity = {
+      ...CreateUserDto,
+      id: this.users.length + 1,
+      password: passwordHash,
+    };
+
+    this.users.push(user);
+
+    //console.log('Senha= ', passwordHash);
+
+    return user;
+  }
+
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.users;
+  }
+}
