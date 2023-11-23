a19552dbbeb4626f1d9c843812add86155049be4
wsverzut <wsverzut@gmail.com>
2023-11-23 15:37:02 -0300

    feat: insert controller address

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { AddressEntity } from './entities/address.entity';
+import { AddressController } from './address.controller';
+import { AddressService } from './address.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AddressEntity])],
+  controllers: [AddressController],
+  providers: [AddressService],
+})
 export class AddressModule {}
---
+++ src/address/address.service.ts
@@ -0,0 +1,25 @@
+import { Repository } from 'typeorm';
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CreateAddressDto } from './dtos/createAddress.dto';
+import { AddressEntity } from './entities/address.entity';
+
+@Injectable()
+export class AddressService {
+  constructor(
+    @InjectRepository(AddressEntity)
+    private readonly addressRepository: Repository<AddressEntity>,
+  ) {}
+
+  async createAddress(
+    createAddressDto: CreateAddressDto,
+    userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressRepository.save({
+      ...createAddressDto,
+      userId,
+    });
+  }
+}
