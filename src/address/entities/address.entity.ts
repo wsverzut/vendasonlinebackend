6a71071a73aab682373c27e3066f5b143205059b
wsverzut <wsverzut@gmail.com>
2023-11-24 16:05:18 -0300

    feat: insert relation many-to-one

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -2,9 +2,13 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { CityEntity } from 'src/city/entities/city.entity';
 
 @Entity({ name: 'address' })
 export class AddressEntity {
@@ -31,4 +35,12 @@ export class AddressEntity {
 
   @UpdateDateColumn({ name: 'updated_at', nullable: false })
   updateAt: Date;
+
+  @ManyToOne(() => UserEntity, (user) => user.addresses)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+  user?: UserEntity;
+
+  @ManyToOne(() => CityEntity, (city) => city.addresses)
+  @JoinColumn({ name: 'city_id', referencedColumnName: 'id' })
+  city?: CityEntity;
 }
